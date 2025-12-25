416683c1efbdab0678435fd8271ab06149ef2b99
Jarspoot12 <101685978+Jarspoot12@users.noreply.github.com>
2025-12-25 15:52:00 -0500

    Clean up comments in supabaseClient.js
    Removed commented line about the key in supabase client.

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -6,7 +6,6 @@ const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    // ESTA ES LA CLAVE:
     // 'localStorage' (default) = Persiste por siempre
     // 'sessionStorage' = Se borra al cerrar la pestaña
     storage: sessionStorage, 
